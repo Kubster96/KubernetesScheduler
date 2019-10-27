7927a06395c6d74bb7e719ef126908fa79a5f69d
kubster96 <jungiewiczjakub@gmail.com>
2019-10-27 16:34:08 +0100

    finally working random custom scheduler

--- scheduler.py
+++ scheduler.py
@@ -6,11 +6,10 @@ import json
 
 from kubernetes import client, config, watch
 
-print(config)
-config.load_kube_config()
+config.load_incluster_config()
 v1=client.CoreV1Api()
 
-scheduler_name = "custom-scheduler"
+scheduler_name = "my-scheduler"
 
 def nodes_available():
     ready_nodes = []
@@ -21,8 +20,7 @@ def nodes_available():
     return ready_nodes
 
 def scheduler(name, node, namespace="default"):
-    body=client.V1Binding()
-        
+
     target=client.V1ObjectReference()
     target.kind="Node"
     target.apiVersion="v1"
@@ -31,10 +29,9 @@ def scheduler(name, node, namespace="default"):
     meta=client.V1ObjectMeta()
     meta.name=name
     
-    body.target=target
-    body.metadata=meta
-    
-    return v1.create_namespaced_binding_binding(name, namespace, body)
+    body=client.V1Binding(target=target, metadata=meta)
+
+    return v1.create_namespaced_binding(namespace=namespace, body=body)
 
 def main():
     g = nodes_available()
@@ -43,10 +40,6 @@ def main():
     for event in w.stream(v1.list_namespaced_pod, "default"):
         if event['object'].status.phase == "Pending" and event['object'].spec.scheduler_name == scheduler_name:
             try:
-                print()
-                print(client)
-                print(event['object'])
-                print()
                 res = scheduler(event['object'].metadata.name, random.choice(nodes_available()))
             except client.rest.ApiException as e:
                 print(json.loads(e.body)['message'])
